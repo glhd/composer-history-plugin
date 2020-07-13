85d0258b5b861cf9fac9f64a70b29c59be16d18f
Chris Morrell <inxilpro@users.noreply.github.com>
2020-07-13 09:37:28 -0400

    Include current branch always

--- src/InteractsWithGit.php
+++ src/InteractsWithGit.php
@@ -14,9 +14,12 @@ trait InteractsWithGit
 	{
 		$branches = explode("\n", trim(shell_exec('git --no-pager branch -a')));
 		
-		return array_map(function($branch) {
-			// $active = 0 === strpos($branch, '*');
+		$branches[] = $this->getGitBranchName();
+		
+		$branches = array_map(function($branch) {
 			return preg_replace('/^\* */', '', $branch);
 		}, $branches);
+		
+		return array_unique($branches);
 	}
 }
